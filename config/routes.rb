956d9d650d1499e8621b577497d6cc8ee5ddb884
John Keith <johnkeith@outlook.com>
2015-03-01 11:02:56 -0500

    add bootstrap styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   root "application#index"
-  
-  # will need to place API endpoints above wildcard below
-  
-  match "*path" => "application#index", via: [:get]
 end
