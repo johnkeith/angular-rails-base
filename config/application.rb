d0336a9b64e6e051f6f5a721fe30fe0be34ceb7c
John Keith <johnkeith@outlook.com>
2015-02-28 19:39:30 -0500

    add vendor/assets/components to assets paths for bower components

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module AngularRailsBase
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.paths << Rails.root.join("vendor", "assets", "components")
     config.assets.paths << Rails.root.join("angular")
   end
 end
